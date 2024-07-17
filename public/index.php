a004e9153554eec206ebbbdf1c6f2614e8efa063
kd-shimat <shimat@st.kobedenshi.ac.jp>
2024-07-17 16:35:14 +0900

    composer実行

--- public/index.php
+++ public/index.php
@@ -1,19 +1,17 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>各ページへのリンク</title>
-    <ul>
-        <?php
-        # このディレクトリにあるdb*.phpのリストを取得しリンクを生成する
-        foreach (glob('*.php') as $file) {
-            echo "<li><a href='{$file}'>{$file}</a></li>";
-        }
-        ?>
-    </ul>
-</head>
-<body>
+<?php
 
-</body>
-</html>
+use Illuminate\Http\Request;
+
+define('LARAVEL_START', microtime(true));
+
+// Determine if the application is in maintenance mode...
+if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+    require $maintenance;
+}
+
+// Register the Composer autoloader...
+require __DIR__.'/../vendor/autoload.php';
+
+// Bootstrap Laravel and handle the request...
+(require_once __DIR__.'/../bootstrap/app.php')
+    ->handleRequest(Request::capture());
