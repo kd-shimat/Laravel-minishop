2bf9fe739a6570a495cade79082318881e2ecb2c
kd-shimat <shimat@st.kobedenshi.ac.jp>
2024-07-23 16:56:34 +0900

    商品に関する機能を実装

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ItemController;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('index');
+})->name('index');
+
+Route::match(['get', 'post'], 'item/{genre?}',[ItemController::class,'index'])->name('item.index');
+
+Route::get('item/show/{item}',[ItemController::class,'show'])->name('item.show');
+
+//Route::post('item',[ItemController::class,'index'])->name('item.index_post');
+Route::post('cart/create',[CartController::class,'create'])->name('cart.create');
