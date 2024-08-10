5a4e851ab7d88fda0214760626fd9b8d278fb206
kd-shimat <shimat@st.kobedenshi.ac.jp>
2024-08-10 16:37:46 +0900

    ルーティングをきれいにしました

--- routes/web.php
+++ routes/web.php
@@ -10,15 +10,12 @@ Route::get('/', function () {
 })->name('index');
 
 Route::match(['get', 'post'], 'item/{genre?}', [ItemController::class, 'index'])->name('item.index');
-
 Route::get('item/show/{item}', [ItemController::class, 'show'])->name('item.show');
+
 Route::get('cart', [CartController::class, 'index'])->name('cart.index');
+Route::post('cart', [CartController::class, 'store'])->name('cart.store');
+Route::put('cart/{cart}',[CartController::class, 'update'])->name('cart.update');
+Route::delete('cart/{cart}',[CartController::class, 'destroy'])->name('cart.destroy');
+
 Route::get('order', [OrderController::class, 'index'])->name('order.index');
-
-//Route::post('item',[ItemController::class,'index'])->name('item.index_post');
-Route::post('cart/store', [CartController::class, 'store'])->name('cart.store');
-
-Route::put('cart/update/{cart}',[CartController::class, 'update'])->name('cart.update');
-
-Route::delete('cart/destroy/{cart}',[CartController::class, 'destroy'])->name('cart.destroy');
 
