b5c9258b35b2373b622ca246e13c5c8aed1a3d14
SATO Daisuke <densuke@fuga.jp>
2024-10-01 05:16:32 +0000

    #3 カートに入れるテストコード(リファクタ)

--- tests/test_minishop_insert_item_to_cart.py
+++ tests/test_minishop_insert_item_to_cart.py
@@ -37,32 +37,34 @@ class TestCase(unittest.TestCase):
 
     # 接続して、ジャンルを選択して商品一覧を取得するテスト
     def test_access(self):
-        self.driver.get("http://web/")
+        driver = self.driver
+        timestamp = self.timestamp
+
+        driver.get("http://web/")
         # ラジオボタン、name=genreの要素から、value=pcを選択
-        genre = self.driver.find_elements(By.NAME, "genre")
+        genre = driver.find_elements(By.NAME, "genre")
         genre[0].click()
-        self.driver.get_screenshot_as_file(f"results/{self.timestamp}-01-select-pc.png")
+        driver.get_screenshot_as_file(f"results/{timestamp}-01-select-pc.png")
 
         # 送信ボタンをクリック
-        self.driver.find_element(By.XPATH, "/html/body/form/input[2]").click()
+        driver.find_element(By.XPATH, "/html/body/form/input[2]").click()
 
         # 5番目の項目の「詳細」リンクをクリックする
-        logger.info(self.driver.find_element(By.XPATH, "/html/body/table/tbody/tr[6]/td[2]").text) # macBook Pro
-        self.assertIn("MacBook Pro", self.driver.find_element(By.XPATH, "/html/body/table/tbody/tr[6]/td[2]").text)
+        logger.info(driver.find_element(By.XPATH, "/html/body/table/tbody/tr[6]/td[2]").text) # macBook Pro
+        self.assertIn("MacBook Pro", driver.find_element(By.XPATH, "/html/body/table/tbody/tr[6]/td[2]").text)
         # テキスト部分をマウスでなぞった感じでテキストの色反転をさせる
-        self.driver.execute_script("arguments[0].style.color='red';", self.driver.find_element(By.XPATH, "/html/body/table/tbody/tr[6]/td[5]"))
-        self.driver.get_screenshot_as_file(f"results/{self.timestamp}-02-list-pc.png")
+        driver.execute_script("arguments[0].style.color='red';", driver.find_element(By.XPATH, "/html/body/table/tbody/tr[6]/td[5]"))
+        driver.get_screenshot_as_file(f"results/{timestamp}-02-list-pc.png")
 
-        link = self.driver.find_element(By.XPATH, "/html/body/table/tbody/tr[6]/td[5]/a")
+        link = driver.find_element(By.XPATH, "/html/body/table/tbody/tr[6]/td[5]/a")
         link.click()
-        self.driver.get_screenshot_as_file(f"results/{self.timestamp}-03-detail-macbook.png")
+        driver.get_screenshot_as_file(f"results/{timestamp}-03-detail-macbook.png")
 
         # カートに入れるためsubmitのボタンをクリックする
-        self.driver.find_element(By.XPATH,"/html/body/form/table/tbody/tr[6]/th/input").click()
-        self.driver.execute_script("arguments[0].style.color='red';", self.driver.find_element(By.XPATH, "/html/body/table/tbody/tr[2]/td[2]"))
-        self.driver.get_screenshot_as_file(f"results/{self.timestamp}-04-insert-to-cart.png")
-        self.assertEqual("MacBook Pro", self.driver.find_element(By.XPATH, "/html/body/table/tbody/tr[2]/td[2]").text)
-
+        driver.find_element(By.XPATH,"/html/body/form/table/tbody/tr[6]/th/input").click()
+        driver.execute_script("arguments[0].style.color='red';", driver.find_element(By.XPATH, "/html/body/table/tbody/tr[2]/td[2]"))
+        driver.get_screenshot_as_file(f"results/{timestamp}-04-insert-to-cart.png")
+        self.assertEqual("MacBook Pro", driver.find_element(By.XPATH, "/html/body/table/tbody/tr[2]/td[2]").text)
 
 
 
